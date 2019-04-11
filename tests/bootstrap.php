dafea063c2c59a585d10b5d56f3bc7295d3b5923
Jon Hinks <projects.ws@jondh.me.uk>
2019-04-11 23:22:06 +0100

    Add a simple test

---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+$projectRoot = realpath(__DIR__ . '/..');
+require $projectRoot . '/vendor/autoload.php';
+
---
+++ tests/unit/DemoTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class DemoTest extends TestCase
+{
+    public function testSomething()
+    {
+         $this->assertTrue(true);
+    }
+}
